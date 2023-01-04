9a79336893ab28c50d7935be180485cc6b212c06
Bartosz Szydłowski <bbartek311@gmail.com>
2023-01-04 01:55:12 +0100

    Added user service logic

--- CarRepairShop.Infrastructure/User/Services/UserService.cs
+++ CarRepairShop.Infrastructure/User/Services/UserService.cs
@@ -1,19 +1,54 @@
 ﻿using CarRepairShop.Application.Common;
+using CarRepairShop.Infrastructure.Identity;
 using CarRepairShop.Infrastructure.User.Interfaces;
 using CarRepairShop.Infrastructure.User.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
 
 namespace CarRepairShop.Infrastructure.User.Services
 {
     public class UserService : IUserService
     {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public UserService(ApplicationDbContext context, UserManager<ApplicationUser> userManager)
+        {
+            _context = context;
+            _userManager = userManager;
+        }
+
         public async Task<ListResponse<UserViewModel>> GetAll()
         {
-            throw new NotImplementedException();
+            var data = await _context.Users.ToListAsync();
+            var mappedData = new List<UserViewModel>();
+
+            foreach (var user in data)
+            {
+                mappedData.Add(new UserViewModel { Id = user.Id, UserName = user.UserName, Email = user.Email });
+            }
+
+            return new ListResponse<UserViewModel>
+            {
+                Data = mappedData,
+                Total = data.Count
+            };
         }
 
         public async Task<Response<UserViewModel>> Get(string id)
         {
-            throw new NotImplementedException();
+            var user = await _userManager.FindByIdAsync(id);
+            var mappedUser = new UserViewModel
+            {
+                Id = user.Id,
+                UserName = user.UserName,
+                Email = user.Email
+            };
+
+            return new Response<UserViewModel>
+            {
+                Data = mappedUser
+            };
         }
 
         public async Task<BaseResponse> Edit(UserViewModel reqeuest)
